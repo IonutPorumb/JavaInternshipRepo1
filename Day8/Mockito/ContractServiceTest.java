f5590d43400e31e24c57ebbb999e29e327221f45
ionut.c.porumb <ionut_porumb@yahoo.com>
2022-05-15 11:16:16 +0300

    A few tests where added to ContractSerciceTest and PersonServiceTest calsses using the Mockito.verify(), Mockito.times(), and  ArgumentCaptor

--- Day8/Mockito/ContractServiceTest.java
+++ Day8/Mockito/ContractServiceTest.java
@@ -2,6 +2,7 @@ package Day8.Mockito;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -26,7 +27,54 @@ class ContractServiceTest {
 
         Mockito.when(mockContractRepository.isEmployed(123456789)).thenReturn(returnedContract);
         boolean resultIsEmployed = contractService.isEmployed(123456789);
+
         assertTrue(resultIsEmployed);
+
+
+    }
+
+    @Test
+    void isEmployedAndReturnsTrue_verifyTheMethodsCall() {
+
+        ContractRepository mockContractRepository = Mockito.mock(ContractRepository.class);
+        ContractService contractService = new ContractService(mockContractRepository);
+
+        Contract returnedContract = new Contract(
+                123456789, LocalDateTime.of(2027, 5, 20, 12, 0));
+
+        Mockito.when(mockContractRepository.isEmployed(123456789)).thenReturn(returnedContract);
+        boolean resultIsEmployed = contractService.isEmployed(123456789);
+
+        assertTrue(resultIsEmployed);
+
+        Mockito.verify(mockContractRepository, Mockito.times(1)).isEmployed(123456789);
+        Mockito.verify(mockContractRepository, Mockito.atMost(0)).getContractList();
+        Mockito.verify(mockContractRepository, Mockito.times(1)).isEmployed(Mockito.anyInt());
+
+
+    }
+
+    @Test
+    void isEmployedAndReturnsTrue_usingArgumentCaptor() {
+
+        ContractRepository mockContractRepository = Mockito.mock(ContractRepository.class);
+        ContractService contractService = new ContractService(mockContractRepository);
+
+        Contract returnedContract = new Contract(
+                123456789, LocalDateTime.of(2027, 5, 20, 12, 0));
+
+        Mockito.when(mockContractRepository.isEmployed(123456789)).thenReturn(returnedContract);
+        boolean resultIsEmployed = contractService.isEmployed(123456789);
+
+        assertTrue(resultIsEmployed);
+
+
+        ArgumentCaptor<Integer> integerArgumentCaptor = ArgumentCaptor.forClass(Integer.class);
+        Mockito.verify(mockContractRepository, Mockito.times(1)).isEmployed(integerArgumentCaptor.capture());
+
+        Integer actualParameter = integerArgumentCaptor.getValue();
+        System.out.println("The parameter used to call the isEmployed() method is: " + actualParameter);
+
 
     }
 
@@ -41,6 +89,27 @@ class ContractServiceTest {
         Mockito.when(mockContractRepository.isEmployed(123456789)).thenReturn(null);
         boolean resultIsEmployed = contractService.isEmployed(123456789);
         assertFalse(resultIsEmployed);
+
+
+    }
+
+    @Test
+    void isEmployedAndReturnsFalse_usingArgumentCapture() {
+
+        ContractRepository mockContractRepository = Mockito.mock(ContractRepository.class);
+        ContractService contractService = new ContractService(mockContractRepository);
+
+
+        Mockito.when(mockContractRepository.isEmployed(123456789)).thenReturn(null);
+        boolean resultIsEmployed = contractService.isEmployed(123456789);
+        assertFalse(resultIsEmployed);
+        ArgumentCaptor<Integer> integerArgumentCaptor = ArgumentCaptor.forClass(Integer.class);
+        Mockito.verify(mockContractRepository, Mockito.times(1)).isEmployed(integerArgumentCaptor.capture());
+
+        Integer parameterValue = integerArgumentCaptor.getValue();
+
+        System.out.println("The parameter used to call the method isEmployed() is : " + parameterValue);
+
 
     }
 }
--- Day8/Mockito/PersonServiceTest.java
+++ Day8/Mockito/PersonServiceTest.java
@@ -3,6 +3,7 @@ package Day8.Mockito;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -29,12 +30,17 @@ public class PersonServiceTest {
         Mockito.when(mockContractService.isEmployed(123456789)).thenReturn(true);
 
 
+//        Mockito.when(personService.findAllEmployedPersons()).thenCallRealMethod();
+//        On the Web app simulation add a few more tests in which you use Mockito.verify(), Mockito.times(), ArgumentCaptor.
+
+
         Assertions.assertEquals(personRepositoryResult, personService.findAllEmployedPersons());
+
 
     }
 
     @Test
-    public void findAllEmployedPersonsFindNoPerson() {
+    public void findAllEmployedPersonsFindAPerson_verifyTheMethodsCall() {
         PersonRepository mockPersonRepository = Mockito.mock(PersonRepository.class);
         ContractService mockContractService = Mockito.mock(ContractService.class);
 
@@ -43,14 +49,107 @@ public class PersonServiceTest {
         List<Person> personRepositoryResult = new ArrayList<>(List.of(
                 new Person(123456789, "Radu Popescu", 32)));
 
+
+        Mockito.when(mockPersonRepository.getPersonList()).thenReturn(personRepositoryResult);
+        Mockito.when(mockContractService.isEmployed(123456789)).thenReturn(true);
+
+
+//        Mockito.when(personService.findAllEmployedPersons()).thenCallRealMethod();
+//        On the Web app simulation add a few more tests in which you use Mockito.verify(), Mockito.times(), ArgumentCaptor.
+
+
+        Assertions.assertEquals(personRepositoryResult, personService.findAllEmployedPersons());
+        Mockito.verify(mockPersonRepository, Mockito.times(1)).getPersonList();
+        Mockito.verify(mockPersonRepository, Mockito.atMost(4)).getPersonList();
+        Mockito.verify(mockContractService, Mockito.times(1)).isEmployed(123456789);
+        Mockito.verify(mockContractService, Mockito.atLeast(1)).isEmployed(123456789);
+
+    }
+
+    @Test
+    public void findAllEmployedPersonsFindingNoPerson() {
+        PersonRepository mockPersonRepository = Mockito.mock(PersonRepository.class);
+        ContractService mockContractService = Mockito.mock(ContractService.class);
+
+        PersonService personService = new PersonService(mockPersonRepository, mockContractService);
+
+        List<Person> personRepositoryResult = new ArrayList<>(List.of(
+                new Person(123456789, "Radu Popescu", 32),
+                new Person(223456789, "Monica Coman", 32)));
+
+
         List<Person> findMethodResult = new ArrayList<>();
 
 
         Mockito.when(mockPersonRepository.getPersonList()).thenReturn(personRepositoryResult);
         Mockito.when(mockContractService.isEmployed(123456789)).thenReturn(false);
+        Mockito.when(mockContractService.isEmployed(223456789)).thenReturn(false);
 
 
         Assertions.assertEquals(findMethodResult, personService.findAllEmployedPersons());
+
+
+    }
+
+    @Test
+    public void findAllEmployedPersonsFindingNoPerson_verifyTheMethodsCall() {
+        PersonRepository mockPersonRepository = Mockito.mock(PersonRepository.class);
+        ContractService mockContractService = Mockito.mock(ContractService.class);
+
+        PersonService personService = new PersonService(mockPersonRepository, mockContractService);
+
+        List<Person> personRepositoryResult = new ArrayList<>(List.of(
+                new Person(123456789, "Radu Popescu", 32),
+                new Person(223456789, "Monica Coman", 32)));
+
+
+        List<Person> findMethodResult = new ArrayList<>();
+
+
+        Mockito.when(mockPersonRepository.getPersonList()).thenReturn(personRepositoryResult);
+        Mockito.when(mockContractService.isEmployed(123456789)).thenReturn(false);
+        Mockito.when(mockContractService.isEmployed(223456789)).thenReturn(false);
+
+
+        Assertions.assertEquals(findMethodResult, personService.findAllEmployedPersons());
+
+        Mockito.verify(mockPersonRepository, Mockito.atLeast(1)).getPersonList();
+        Mockito.verify(mockContractService, Mockito.times(1)).isEmployed(123456789);
+
+
+    }
+
+
+    @Test
+    public void findAllEmployedPersonsWithMorePersons_usingArgumentCapture() {
+        PersonRepository mockPersonRepository = Mockito.mock(PersonRepository.class);
+        ContractService mockContractService = Mockito.mock(ContractService.class);
+
+        PersonService personService = new PersonService(mockPersonRepository, mockContractService);
+
+        List<Person> personRepositoryResult = new ArrayList<>(List.of(
+                new Person(123456789, "Radu Popescu", 32),
+                new Person(223456789, "Monica Coman", 32),
+                new Person(323456789, "Radu Mazare", 42)));
+
+
+        Mockito.when(mockPersonRepository.getPersonList()).thenReturn(personRepositoryResult);
+        Mockito.when(mockContractService.isEmployed(123456789)).thenReturn(true);
+        Mockito.when(mockContractService.isEmployed(323456789)).thenReturn(true);
+
+        personRepositoryResult.remove(1);
+
+        Assertions.assertEquals(personRepositoryResult, personService.findAllEmployedPersons());
+//        Assertions.assertTrue(personService.findAllEmployedPersons().containsAll(findMethodResult));
+
+        Mockito.verify(mockPersonRepository, Mockito.atLeast(1)).getPersonList();
+        Mockito.verify(mockContractService, Mockito.times(2)).isEmployed(Mockito.anyInt());
+        ArgumentCaptor<Integer> integerArgumentCaptor = ArgumentCaptor.forClass(Integer.class);
+        Mockito.verify(mockContractService, Mockito.times(2)).isEmployed(integerArgumentCaptor.capture());
+
+        List<Integer> actualParameters = new ArrayList<>();
+        actualParameters = integerArgumentCaptor.getAllValues();
+        System.out.println("The parameters used to call the isEmployed() method are: " + actualParameters);
 
 
     }
@@ -77,6 +176,10 @@ public class PersonServiceTest {
 
         Assertions.assertEquals(expectedResult, personServiceResult);
 
+        Mockito.verify(mockPersonRepository, Mockito.times(1)).findByName(personName);
+        Mockito.verify(mockContractService, Mockito.times(0)).isEmployed(123456789);
+
+
     }
 
     @Test
@@ -97,6 +200,10 @@ public class PersonServiceTest {
         List<Person> personServiceResult = personService.findByName(personName);
 
         Assertions.assertEquals(expectedResult, personServiceResult);
+
+        Mockito.verify(mockPersonRepository, Mockito.times(1)).findByName(personName);
+        Mockito.verify(mockPersonRepository, Mockito.atMostOnce()).findByName(personName);
+
 
     }
 }
